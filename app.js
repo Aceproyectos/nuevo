8f3872c7e47dceb21312110bbd3482667093382a
Aceproyectos <115713715+Aceproyectos@users.noreply.github.com>
2023-02-02 15:28:03 -0500

    probando sin http

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-const http = require('http');
+const express = require("express");
+const { engine } = require("express/lib/application");
+const { render, json } = require("express/lib/response");
+const morgan = require("morgan");
+const path = require("path");
+const app = express();
 
-const hostname = 'localhost';
-const port = 3000;
+app.use(express.static(path.join(__dirname,'public')));
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express(json));
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Worl5546454d!\n');
+app.set('port', 80);
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname,'views/'));
+
+app.use(require('./router/routes'));
+app.use((err, req, res, next) => {
+    res.send({ err: err.message });
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(app.get('port'), () => {
+    console.log("Se esta ultilizando el puerto",app.get('port'));
+}); 
